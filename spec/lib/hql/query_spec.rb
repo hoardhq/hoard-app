7ca5eee0dd09a6dc4ec134144366bcbbd872cfcb
Marc Qualie <marc@marcqualie.com>
2015-07-28 11:58:21 +0100

    Refactor test case to ensure HQL test cases are easier to write

--- spec/lib/hql/query_spec.rb
+++ spec/lib/hql/query_spec.rb
@@ -2,70 +2,44 @@ require 'hql/query'
 
 describe HQL::Query do
 
-  describe "#valid?" do
-    context "with single condition" do
-      context "in single quotes" do
-        subject { HQL::Query.new("host = 's1.example.org'").valid? }
-        it do
-          expect(subject).to eq true
-        end
-      end
-      context "in double quotes" do
-        subject { HQL::Query.new("host = \"s1.example.org\"").valid? }
-        it do
-          expect(subject).to eq true
-        end
-      end
-      context "that is invalid" do
-        subject { HQL::Query.new("host = 's1.example.org").valid? }
-        it do
-          expect(subject).to eq false
-        end
-      end
-    end
-  end
+  queries = [
+    {
+      query: "host = 's1.example.org'",
+      sql: "data->>'host' = 's1.example.org'",
+      valid: true,
+    },
+    {
+      query: "host = \"s1.example.org\"",
+      sql: "data->>'host' = 's1.example.org'",
+      valid: true,
+    },
+    {
+      query: "host = \"s1.example.org",
+      sql: nil,
+      valid: false,
+    },
+    {
+      query: "host = 's1.example.org' path = '/test'",
+      sql: "data->>'host' = 's1.example.org' AND data->>'path' = '/test'",
+      valid: true,
+    }
+  ]
 
-  describe "#to_sql" do
-    context "with single condition" do
-      context "in single quotes" do
-        subject { HQL::Query.new("host = 's1.example.org'").to_sql }
-        it do
-          expect(subject).to eq "data->>'host' = 's1.example.org'"
-        end
-      end
-      context "in double quotes" do
-        subject { HQL::Query.new("host = \"s1.example.org\"").to_sql }
-        it do
-          expect(subject).to eq "data->>'host' = 's1.example.org'"
-        end
-      end
-      context "that is invalid" do
-        subject { HQL::Query.new("host = 's1.example.org").to_sql }
-        it do
-          expect(subject).to eq nil
-        end
-      end
-    end
-    context "with multiple conditions" do
-      context "in single quotes" do
-        subject { HQL::Query.new("host = 's1.example.org' path = '/test'").to_sql }
-        it do
-          expect(subject).to eq "data->>'host' = 's1.example.org' AND data->>'path' = '/test'"
-        end
-      end
-      context "in double quotes" do
-        subject { HQL::Query.new("host = \"s1.example.org\" path = \"/test\"").to_sql }
-        it do
-          expect(subject).to eq "data->>'host' = 's1.example.org' AND data->>'path' = '/test'"
-        end
-      end
-      context "where one is invalid" do
-        subject { HQL::Query.new("host = 's1.example.org' AND path = /test'").to_sql }
-        it do
-          expect(subject).to eq "data->>'host' = 's1.example.org'"
-        end
-      end
-    end
-  end
+  queries.each do |options|
 
+    describe "#valid?" do
+      it "#{options[:query]} should be valid" do
+        query = HQL::Query.new(options[:query])
+        expect(query.valid?).to eq options[:valid]
+      end
+    end
+
+    describe "#to_sql" do
+      it do
+        query = HQL::Query.new(options[:query])
+        expect(query.to_sql).to eq options[:sql]
+      end
+    end
+
+  end
 end
