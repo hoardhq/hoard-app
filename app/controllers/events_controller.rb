873761932eac25e337485bbd87c98197016ad43b
Marc Qualie <marc@marcqualie.com>
2015-07-28 14:53:49 +0100

    Split event data into table view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,6 +11,7 @@ class EventsController < ApplicationController
       @events = @events.where(@hql_query.to_sql) if @hql_query.valid?
     end
     @events = @events.order(id: :desc).limit(params[:limit].presence || 25).offset(params[:offset].presence || 0)
+    @columns = @events.map { |row| row.data.keys }.flatten.uniq.sort
   end
 
 end
