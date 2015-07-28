1d6554b4b312bae5f1a0c6128928267d055f318b
Marc Qualie <marc@marcqualie.com>
2015-07-28 17:44:32 +0100

    Create new events directly

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,8 @@ require 'hql/query'
 
 class EventsController < ApplicationController
 
+  skip_before_action :verify_authenticity_token, only: :create
+
   def index
     @stream = Stream.find_by(slug: params[:stream])
     @events = Event.all
@@ -12,6 +14,22 @@ class EventsController < ApplicationController
     end
     @events = @events.order(id: :desc).limit(params[:limit].presence || 25).offset(params[:offset].presence || 0)
     @columns = @events.map { |row| row.data.keys }.flatten.uniq.sort
+  end
+
+  def create
+    payloads = JSON.parse request.raw_post
+    unless payloads.is_a? Array
+      payloads = [payloads]
+    end
+    events = []
+    payloads.each do |payload|
+      stream = Stream.select(:id).find_by(slug: payload.delete('stream'))
+      if stream.present?
+        payload['stream_id'] = stream.id
+        events.push Event.create(payload)
+      end
+    end
+    render json: events.to_json, status: 201
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root "streams#index"
 
-  resources :events, only: [:index]
+  resources :events, only: [:index, :create]
   resources :streams, only: [:index, :show]
 
 end
