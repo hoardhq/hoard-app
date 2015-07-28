ffb9e4e8593842d3bc745a1d0fa820c3baaa98fa
Marc Qualie <marc@marcqualie.com>
2015-07-28 10:08:35 +0100

    Indicate if HQL query is valid or not in UI

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,11 +6,11 @@ class EventsController < ApplicationController
     @stream = Stream.find_by(slug: params[:stream])
     @events = Event.all
     @events = @events.where(stream_id: @stream.id) if @stream.present?
-    if params[:hql]
-      hql_query = HQL::Query.new params[:hql]
-      @events = @events.where(hql_query.to_sql)
+    if params[:hql].present?
+      @hql_query = HQL::Query.new params[:hql]
+      @events = @events.where(@hql_query.to_sql) if @hql_query.valid?
     end
-    @events = @events.order(id: :desc).limit(params[:limit] || 50).offset(params[:offset] || 0)
+    @events = @events.order(id: :desc).limit(params[:limit].presence || 25).offset(params[:offset].presence || 0)
   end
 
 end
