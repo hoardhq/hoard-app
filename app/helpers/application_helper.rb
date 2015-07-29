50d0f19d35205a76eeab9520ba4e3c71f5c4a556
Marc Qualie <marc@marcqualie.com>
2015-07-29 19:32:28 +0100

    Add nav_link helper to easier active link display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def nav_link(path = nil, options = nil, &block)
+    li_options = current_page?(path) ? { class: "active" } : {}
+    content_tag(:li, li_options) do
+      link_to(path, options, &block)
+    end
+  end
+
 end
