b1a49c6068bc6d0ec45db14cc0dcccfa27e0be32
Marc Qualie <marc@marcqualie.com>
2015-08-03 23:32:38 +0100

    Improve seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,22 @@
 Stream.destroy_all
 User.destroy_all
 
+puts "Creaintg Users"
 User.create!(email: 'user@example.com', password: 'password')
 
+puts "Creating Streams"
 Stream.create!(name: 'Example #1', slug: 'example-1')
 Stream.create!(name: '/dev/null', slug: 'devnull')
 
 # Access Log Stream
 stream = Stream.create(name: 'Access Log', slug: 'accesslog')
-(1..1000).each do |n|
+puts "Creating Events"
+(0..999).each do |n|
+  print "."
+  puts "" if n % 100 == 99
   stream.events.create(
     data: {
-      uuid: "#{n.to_s.ljust 4, '0'}-#{(rand * 10000000).ceil.to_s.ljust 8, '0'}",
+      uuid: SecureRandom.uuid,
       host: "s#{(rand * 10).ceil}.example.org",
       path: "/users/#{(rand * 10000).ceil}",
       status: [200, 301, 302, 404, 500, 503].sample,
