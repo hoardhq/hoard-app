441bb6f9b44000adb8fc77041a70e3e7615a57d6
Marc Qualie <marc@marcqualie.com>
2015-08-05 20:43:42 +0100

    Status validations

--- app/models/report_result.rb
+++ app/models/report_result.rb
@@ -2,4 +2,10 @@ class ReportResult < ActiveRecord::Base
 
   belongs_to :report
 
+  validates :status, inclusion: { in: %w(queued running complete) }
+
+  def complete?
+    status === 'complete'
+  end
+
 end
