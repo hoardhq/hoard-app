c8396294ab6c2b06dccf9266f0f0d512302e7408
Marc Qualie <marc@marcqualie.com>
2015-08-07 00:14:01 +0100

    Don't use custom Redis initialiser

--- config/initializers/redis.rb
+++
@@ -1,9 +0,0 @@
-REDIS_URL = "redis://#{ENV['REDIS_PORT_6379_TCP_ADDR'].presence || '127.0.0.1'}:6379"
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: REDIS_URL }
-end
-
-Sidekiq.configure_server do |config|
-  config.redis = { url: REDIS_URL }
-end
