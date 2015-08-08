82bad1de9114eb2f0c708d96f567320f1461d69d
Marc Qualie <marc@marcqualie.com>
2015-08-08 15:19:49 +0100

    Increased schedule period selection

--- app/controllers/importers_controller.rb
+++ app/controllers/importers_controller.rb
@@ -1,5 +1,7 @@
 class ImportersController < ApplicationController
 
+  helper_method :schedule_periods
+
   def create
     @importer = Importer.new(params.require(:importer).permit(:stream_id, :provider, :endpoint, :schedule))
     if @importer.save
@@ -31,6 +33,22 @@ class ImportersController < ApplicationController
     end
     flash[:success] = "Your import has been scheduled"
     redirect_to importers_path
+  end
+
+  private
+
+  def schedule_periods
+    {
+      "Every minute" => 60,
+      "Every 5 minutes" => 300,
+      "Every 15 minutes" => 900,
+      "Every 30 minutes" => 1800,
+      "Every hour" => 3600,
+      "Every 6 hours" => 21600,
+      "Every day" => 86400,
+      "Every 7 days" => 604800,
+      "Every 30 days" => 2592000,
+    }
   end
 
 end
